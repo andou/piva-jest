99086d9273c321b3c8cbd56081d9f9af2cef5c22
Andou <antonio.pastorino@gmail.com>
2021-01-15 14:12:47 +0100

    :wrench: Aggiunti test per PIVE non valide

--- partita_iva.test.js
+++ partita_iva.test.js
@@ -1,9 +1,16 @@
 const partita_iva = require('./partita_iva');
 const pive_valide = require('./pive_valide');
+const pive_non_valide = require('./pive_non_valide');
 
 pive_valide.forEach(function (e) {
     test('Check ' + e, () => {
         expect(partita_iva(e)).toBe(true);
+    });
+});
+
+pive_non_valide.forEach(function (e) {
+    test('Check ' + e, () => {
+        expect(partita_iva(e)).toBe(false);
     });
 });
 
